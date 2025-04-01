16e8b4a0a35ffcb79e38c70632d80412e6cbb474
bobinuts <pellicano.theo@orange.fr>
2025-04-01 11:02:39 +0200

    modif encore

--- src/app.py
+++ src/app.py
@@ -1,10 +1,20 @@
 from flask import Flask
 from flask import request
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, {ip}, your User-Agent is: {ua}</p>".format(ip=request.remote_addr, ua=request.headers.get('User-Agent'))
+    return (
+        "<p>Hello, {ip}, your User-Agent is: {ua}</p>"
+        .format(
+            ip=request.remote_addr,
+            ua=request.headers.get("User-Agent")
+        )
+    )
 
-app.run(host='0.0.0.0', port=8080)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
